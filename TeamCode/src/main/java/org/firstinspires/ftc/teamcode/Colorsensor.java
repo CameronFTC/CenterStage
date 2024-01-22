3c59e1ec10e4c9a4930e848661eb322d1289a034
HenryRal <113390911+HenryRal@users.noreply.github.com>
2024-01-22 15:04:53 -0600

    henryColorStuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Colorsensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Colorsensor.java
@@ -20,9 +20,23 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
         // While the Op Mode is running, update the telemetry values.
         while (opModeIsActive()) {
+            //nothing R27 G24 B16
+            //green R136 G251 B108 to R328 G644 B277
+            //white R1907 G2209 B1852
+            //yellow R786 G620 B284 to R1783 G1499 B662
+            //purple R1122 G1286 B1520
+            String col = "";
+            double RtoG = color.red() / color.green();
+            double GtoB = color.green() / color.blue();
+            double BtoR = color.blue() / color.red();
+
+
             telemetry.addData("Red", color.red());
             telemetry.addData("Green", color.green());
             telemetry.addData("Blue", color.blue());
+            telemetry.addData("RtoG", RtoG);
+            telemetry.addData("GtoB", GtoB);
+            telemetry.addData("BtoR", BtoR);
 
             telemetry.update();
         }
